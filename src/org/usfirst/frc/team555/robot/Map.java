58fd9b86b025b2cb55a9df095c52d9491914394b
hooper-dylan <Jack.hymowitz@gmail.com>
2016-01-15 16:09:48 -0500

    Created map for the robot ports

---
+++ src/org/usfirst/frc/team555/robot/Map.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+public class Map
+{
+    public static final int[][] MOTOR_VALUES={
+        //{motorPort,encoderPort1,encoderPort2},
+        {0,1,2},
+        {3,4,5},
+        {6,7,8},
+        {9,10,11},
+    };
+}
