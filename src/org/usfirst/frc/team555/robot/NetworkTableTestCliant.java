d5d5e4ac16c3d25846f2d26cb69a2c2d6b194559
hooper-dylan <jack.hymowitz@gmail.com>
2016-01-16 18:40:31 -0500

    Added NetworkTable testing functionality

---
+++ src/org/usfirst/frc/team555/robot/NetworkTableTestCliant.java
@@ -0,0 +1,8 @@
+package org.usfirst.frc.team555.robot;
+
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+
+public class NetworkTableTestCliant 
+{
+	//http://wpilib.screenstepslive.com/s/4485/m/26401/l/255416-creating-a-custom-control-using-java
+}
