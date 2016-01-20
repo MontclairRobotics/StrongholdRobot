a7e5f861aee0bef445038216e4d657399b7f133b
iLoveCode13 <j.wish.2012@gmail.com>
2016-01-20 12:16:22 -0500

    Fixed typo, added comments

--- src/org/usfirst/frc/team555/robot/NavXGyro.java
+++ src/org/usfirst/frc/team555/robot/NavXGyro.java
@@ -3,19 +3,27 @@ import com.kauailabs.navx.frc.AHRS;
 
 public class NavxGyro {
     
+    // AHRS reference from wherever NavXGyro is initialized 
     private AHRS ahrs;
     
     private public NavXGyro(AHRS ahrs) {
         this.ahrs = ahrs;
-        ahrs.reset(); // Resets gyro readings to 0
+         // Resets gyro readings to 0 (starting orientation)
+        ahrs.reset();
     }
+    
+    /* 
+    NOTE: - getX(), getY(), and getZ() are measured in degrees
+          - Range: [-180°, 180°) 
+          - ahrs.getPitch(), ahrs.getRoll(), and ahrs.getYaw() return type float
+    */
     
     public double getX() {
         return (double)ahrs.getPitch();
     }
     
     public double getY() {
-        return (double)ahrs.getRool();
+        return (double)ahrs.getRoll(); // Ignore documentation; there is a typo
     }
     
     public double getZ() {
