7d53bd6f47ec7d75120077df2aeb7b3cfa57137b
iLoveCode13 <j.wish.2012@gmail.com>
2016-01-20 20:40:15 -0500

    Updated conversion, added functions, comments
    The G-force conversion factor is now an average of gravitational readings from the pole and the equator (Wikipedia).
    The new velocity functions are listed as "experimental"; measured in meters per second.

--- src/org/usfirst/frc/team555/robot/NavXAccelerometer.java
+++ src/org/usfirst/frc/team555/robot/NavXAccelerometer.java
@@ -3,22 +3,44 @@ import com.kauailabs.navx.frc.AHRS;
 
 public class NavXAccelerometer {
     
-    private AHRS ahrs;
+    private AHRS ahrs; // References the NavX computer
     
     public NavXAccelerometer(AHRS ahrs) {
         this.ahrs = ahrs;
     }
     
-    public getX() {
-        return (double)ahrs.getWorldLinearAccelX() / 9.8;
+    /*
+    NOTE: - acceleration functions are measured in "m/s^2"
+          - ahrs functions are of type float
+    */
+    
+    public getAccelX() {
+        return (double)ahrs.getWorldLinearAccelX() / 9.806;
     }
     
-    public getY() {
-        return (double)ahrs.getWorldLinearAccelX() / 9.8;
+    public getAccelY() {
+        return (double)ahrs.getWorldLinearAccelX() / 9.806;
     }
     
-    public getZ() {
-        return (double)ahrs.getWorldLinearAccelX() / 9.8;
+    public getAccelZ() {
+        return (double)ahrs.getWorldLinearAccelX() / 9.806;
+    }
+    
+    /*
+    NOTE: - velocity functions are measured in "m/s"
+          - ahrs functions are of type float
+    */
+    
+    public getVelocX() {
+        return (double)ahrs.getVelocityX();
+    }
+    
+    public getVelocY() {
+        return (double)ahrs.getVelocityY();
+    }
+    
+    public getVelocZ() {
+        return (double)ahrs.getVelocityZ();
     }
     
 }
