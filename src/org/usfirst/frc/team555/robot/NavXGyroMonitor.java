0e2f5f16c7e9a4a2ac35733003d43ed49713f3f3
iLoveCode13 <j.wish.2012@gmail.com>
2016-01-23 13:20:21 -0500

    Update NavXGyroMonitor.java
    I have created a bunch of private constants for the different intervals regarding Direction and Tilt (they are of type DoubleRange)
    Renamed the class as "NavXGyroMonitor"

--- src/org/usfirst/frc/team555/robot/NavXGyroMonitor.java
+++ src/org/usfirst/frc/team555/robot/NavXGyroMonitor.java
@@ -1,7 +1,19 @@
 package org.usfirst.frc.team555.robot;
-import com.kauailabs.navx.frc.AHRS;
 
-public class GyroMonitor {
+import com.kauailabs.navx.frc.AHRS;
+import com.ni.vision.NIVision.Range;
+
+public class NavXGyroMonitor {
+    
+    private static final DoubleRange FORWARD_INTERVAL =   new Range(-45.0, 45.0); // Robot is facing the enemy tower
+    private static final DoubleRange LEFT_INTERVAL =      new Range(45.0, 135.0); // Robot is facing driver's left
+    private static final DoubleRange RIGHT_INTERVAL =     new Range(-135.0, -45.0); // Robot is facing driver's right
+    private static final DoubleRange BACKWARD_INTERVAL =  new Range.between(-45.0, 45.0); // Robot is facing drivers station
+    
+    private static final DoubleRange NOMINAL_INTERVAL =       new DoubleRange(0.0, 5.0); // This represents whatever difference might be caused by regular jostling 
+    private static final DoubleRange INCLINED_INTERVAL =      new DoubleRange(5.0, 20.0);
+    private static final DoubleRange NEAR_FATAL_INTERVAL =    new DoubleRange(20.0, 30.0); // TODO: - Replace 30° with whatever the tipping point of the robot is
+    private static final DoubleRange FATAL_INTERVAL =         new DoubleRange(30.0, 180.0);
     
     public enum Direction {
         FORWARD, LEFT, RIGHT, BACKWARD;
@@ -13,27 +25,27 @@ public class GyroMonitor {
     
     NavXGyro gyro; // Reference to NavX gyroscope
     
-    public GyroMonitor(NavXGyro gyro) {
+    public NavXGyroMonitor(NavXGyro gyro) {
         this.gyro = gyro;
     }
     
     public void display() {
         SmartDashboard.putString("Direction", getDirection().toString() + 
-            "\nZ-axis: " + String.valueOf(gyro.getZ()) + "°");
+            "\n\tZ-axis: " + String.valueOf(gyro.getZ()) + "°");
         SmartDashboard.putString("Tilt", getTilt().toString() + 
-            "\nX-axis: " + String.valueOf(gyro.getX()) + "°, Y-axis: " + String.valueOf(gyro.getY()) + "°");
+            "\n\tX-axis: " + String.valueOf(gyro.getX()) + "°, Y-axis: " + String.valueOf(gyro.getY()) + "°");
     }
     
     public Direction getDirection() {
         double zValue = gyro.getZ();
         
-        if (zValue >= -45 && zValue < 45)
+        if (FORWARD_INTERVAL.containsDouble(zValue))
             direction = Direction.FORWARD;
-        else if (zValue >= 45 && zValue < 135)
+        else if (LEFT_INTERVAL.containsDouble(zValue))
             direction = Direction.LEFT;
-        else if (zValue >= -135 && zValue < -45)
+        else if (RIGHT_INTERVAL.containsDouble(zValue))
             direction = Direction.RIGHT;
-        else
+        else if (BACKWARD_INTERVAL.containsDouble(zValue))
             direction = Direction.BACKWARD;
     }
     
@@ -42,13 +54,13 @@ public class GyroMonitor {
         double yMag = Math.abs(gyro.getY());
         double greatestTilt = (xMag > yMag) ? xMag : yMag;
         
-        if (greatestTilt < 5) // This represents whatever difference might be caused by regular jostling 
+        if (NOMINAL_INTERVAL.containsDouble(greatestTilt))
             tilt = Tilt.NOMINAL;
-        else if (greatestTilt < 20)
+        else if (INCLINED_INTERVAL.containsDouble(greatestTilt))
             tilt = Tile.INCLINED;
-        else if (greatistTilt < 30) // TODO: - Replace 30° with whatever the tipping point of the robot is
+        else if (NEAR_FATAL_INTERVAL.containsDouble(greatestTilt))
             tilt = Tilt.NEAR_FATAL;
-        else
+        else if (FATAL_INTERVAL.containsDouble(greatestTilt))
             tilt = Tilt.FATAL;
     }
     
