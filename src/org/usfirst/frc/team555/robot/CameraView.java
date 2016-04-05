fcede9084be9271f10827781545b566821c74685
iLoveCode13 <j.wish.2012@gmail.com>
2016-04-05 18:04:24 -0400

    Just changed a short switch statement to if-else

--- src/org/usfirst/frc/team555/robot/CameraView.java
+++ src/org/usfirst/frc/team555/robot/CameraView.java
@@ -4,15 +4,15 @@ import com.ni.vision.VisionException;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.vision.USBCamera;
 
-enum state {cam1, cam2}
+enum states {cam1, cam2}
 
-public class CameraView extends StateMachine<state> {
+public class CameraView extends StateMachine<states> {
   
   public CameraServer server;
   USBCamera camera1, camera2;
   
   public CameraView(String name1, String name2) {
-    super(state.cam1);
+    super(states.cam1);
     
     try {
       camera1 = USBCamera(name1);
@@ -20,24 +20,20 @@ public class CameraView extends StateMachine<state> {
       
       server = CameraServer.getInstance();
       server.startAutomaticCapture(camera);
-    } catch (VisionException ex){
+    } catch (VisionException ex) {
       ex.printStackTrace();
-  	}
+    }
   }
   
-  public state calculateNextState() {
-    return (currentState == cam1) ? state.cam2 : state.cam1;
+  public states calculateNextState() {
+    return (currentState == cam1) ? .cam2 : .cam1;
   }
   
-  public void executeTransition(state next) {
-    switch (next) {
-      case .cam1:
-        server.startAutomaticCapture(cam2);
-        break;
-      case .cam2:
-        server.startAutomaticCapture(cam1);
-        break;
-      default: break;
+  public void executeTransition(states next) {
+    if (next == .cam1) {
+      server.startAutomaticCapture(cam1);
+    } else {
+      server.startAutomaticCapture(cam2);
     }
   }
   
