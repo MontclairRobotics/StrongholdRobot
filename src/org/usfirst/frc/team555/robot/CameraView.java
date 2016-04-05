7b889c8de14d2dc602b410905b6b7c87c97bc4fa
iLoveCode13 <j.wish.2012@gmail.com>
2016-04-05 18:38:00 -0400

    CameraView is finished, ready to be tested

--- src/org/usfirst/frc/team555/robot/CameraView.java
+++ src/org/usfirst/frc/team555/robot/CameraView.java
@@ -4,6 +4,18 @@ import com.ni.vision.VisionException;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.vision.USBCamera;
 
+/*
+ * CameraView.java
+ *
+ * Joshua Rapoport 2016
+ *
+ * This is a state machine designed to flip between two USB cameras (identified by name).
+ * Accomplished by pressing the buttons on the Joystick identified as 3 and 4.
+ * Implemented in Robot.java
+ * Results should appear in SmartDashboard as a live feed of either camera.
+ * 
+ */
+
 enum states {cam1, cam2}
 
 public class CameraView extends StateMachine<states> {
@@ -27,6 +39,14 @@ public class CameraView extends StateMachine<states> {
   
   public states calculateNextState() {
     return (currentState == cam1) ? .cam2 : .cam1;
+  }
+  
+  public void update() {
+    if (Control.getButton(Control.DRIVE_STICK, 3) && currentState != .cam1) {
+      executeTransition(.cam1);
+    } else if (Control.getButton(Control.DRIVE_STICK, 4) && currentState != .cam2) {
+      executeTransition(.cam2);
+    }
   }
   
   public void executeTransition(states next) {
